792abf83da3f1b48a04a311913d1e89a8c02345b
ThangTruong <thangtm6@fsoft.com.vn>
2017-03-09 10:59:58 +0700

    Seperate DataSource and Delegate to Extension of TableViewController

---
+++ SplitDelegateDataSource/TableVC+DataSource.swift
@@ -0,0 +1,26 @@
+//
+//  DataSource.swift
+//  SplitDelegateDataSource
+//
+//  Created by Trương Thắng on 3/9/17.
+//  Copyright © 2017 Trương Thắng. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - Table view data source
+
+extension TableViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return students.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = students[indexPath.row]
+        return cell
+    }
+}
